44573c9a14cf04cb39bbab9bb9f80dc57bc10866
zjfcool <2568696196@qq.com>
2022-06-21 14:12:39 +0800

    build: dts build

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,31 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
-
+import pkg from "./package.json";
+const pkgName = pkg.name;
+const bundleDirName = "dist";
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: "dist/bundle.esm.js",
+      file: `${bundleDirName}/${pkgName}.esm.js`,
       format: "esm",
     },
     {
-      file: "dist/bundle.unpkg.js",
+      file: `${bundleDirName}/${pkgName}.unpkg.js`,
       format: "iife",
       name: "TEST",
+    },
+    {
+      file: `${bundleDirName}/${pkgName}.esm.min.js`,
+      format: "esm",
+      plugins: [terser()],
+    },
+    {
+      file: `${bundleDirName}/${pkgName}.unpkg.min.js`,
+      format: "iife",
+      name: "TEST",
+      plugins: [terser()],
     },
   ],
   plugins: [resolve(), commonjs(), typescript()],
